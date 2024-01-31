600feb600bf92a4e26b632ca763276cf9adc2e02
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-31 10:08:28 +0100

    add category and product models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,16 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Category extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        "name",
-        "description",
-        "quantity",
-        "price",
-        "category_id"
+        "category"
     ];
-
 }
