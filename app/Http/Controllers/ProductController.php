684babb94a242de87093fc3ad2e30189a454ab12
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-31 11:29:29 +0100

    add edit method to the product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,9 +18,16 @@ class ProductController extends Controller
 
     function store(Request $request)
     {
+        $request->validate([
+            'name' => 'required|unique:products',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'quantity' => 'required|numeric',
+            'category' => 'required|numeric'
+        ]);
         $product = new Product;
         $product->name = $request->name;
-        $product->description = $request->description; 
+        $product->description = $request->description;
         $product->price = $request->price;
         $product->quantity = $request->quantity;
         $product->category = $request->category;
@@ -40,21 +47,41 @@ class ProductController extends Controller
         return view("pages/product_detail", compact("product_data"));
     }
 
-    function delete($id) {
+    function delete($id)
+    {
         $product = Product::find($id);
         $product->delete();
         return redirect('products')->with('status', 'Product Has Been deleted');
     }
-    
-    function edit($id) {
+
+    function edit($id)
+    {
         $product = Product::find($id);
-        $category = Category::find($product->category);
         $categories = Category::all();
         $data = [
             "product_data" => $product,
-            "category" => $category,
             "categories" => $categories
         ];
         return view("pages.edit", compact("data"));
     }
+
+    function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'quantity' => 'required|numeric',
+            'category' => 'required|numeric'
+        ]);
+        $product = Product::find($id);
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->category = $request->category;
+        $product->save();
+        return redirect('products')->with('status', 'Product Form Data Has Been Updated');
+    }
 }
+
