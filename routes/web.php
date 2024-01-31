6d53d993a3cab1b6d95dd8a2cbbc1dc1da2cf5cb
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-31 10:06:43 +0100

    update routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +14,15 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('pages.home');
 });
+
+Route::get("/add", [CategoryController::class, "index"])->name("category.index");
+Route::post("store-form", [ProductController::class, "store"]);
+
+Route::get("products", [ProductController::class, "index"]);
+
+Route::get("edit/{id}", [ProductController::class, "edit"]);
+
+Route::get("delete/{id}", [ProductController::class, "delete"]);
